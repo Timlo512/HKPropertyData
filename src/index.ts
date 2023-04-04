115d8c1385f8267e3070b064bc11f174ac0cafbf
Timothy Lo <timothy@qupital.com>
2023-04-04 10:36:03 +0800

    init commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
 
+const DEFAULT_PAGE = 7;
+const BASE_URL = 'https://28hse.com/'
+
 async function main() {
-    // const url = 'https://google.com';
-    const url = 'https://www.28hse.com/';
+
+    // parse argument
+    const typeOfData = process.argv[2];
+    const page = process.argv[3];
+    // Backlog Validation of the argument
+
+    let url = BASE_URL + typeOfData;
+    console.log(url);
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
-
-    console.log($)
+    console.log($.html());
+    
 
 }
 
